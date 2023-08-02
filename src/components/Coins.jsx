3d58e5185d064f420182ec47ba9197f83ee7e8fb
Kartik Mane <kartik07mane@gmail.com>
2023-08-02 19:26:46 +0530

    First Commit

---
+++ src/components/Coins.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react'
+import { server } from '../index'
+import axios from 'axios'
+import { Container, HStack, Heading, VStack, Image, Text, Button, RadioGroup, Radio } from '@chakra-ui/react'
+import Loader from './Loader'
+import ErrorComponent from './ErrorComponent'
+import CoinCard from './CoinCard'
+
+const Coins = () => {
+
+        const [coins, setCoins] = useState([])
+        const [loading, setLoading] = useState(true)
+        const [error, setError] = useState(false)
+        const [page, setPage] = useState(1)
+        const [currency, setCurrency] = useState("inr")
+
+        const currencySymbol = currency === "inr" ? "₹" : currency === "eur" ? "€" : "$" 
+
+        const changePage = (page) => {
+            setPage(page)
+            setLoading(true)
+        } 
+
+        const btns = new Array(132).fill(1)
+
+        useEffect(() => {
+            const fetchCoins = async () => {
+                try{
+                    const { data } = await axios.get(`
+                        ${server}/coins/markets?vs_currency=${currency}&page=${page}`
+                    )
+                    console.log(data)
+                    setCoins(data)
+                    setLoading(false)
+                }
+                catch(error){
+                    setPage(true)
+                    setLoading(false)
+                }
+            }
+            fetchCoins()
+        }, [currency, page])
+
+        if(error) return <ErrorComponent message={"Error While Fetching coins"}/>
+    return (
+        <Container maxW={"container.xl"}>
+            {loading ? (
+                <Loader/>
+            ) : (
+                <>
+                    <RadioGroup value={currency} onChange={setCurrency}>
+                        <HStack spacing={"4"}>
+                            <Radio value={"inr"}>INR</Radio>
+                            <Radio value={"usd"}>USD</Radio>
+                            <Radio value={"eur"}>EUR</Radio>
+                        </HStack>
+                    </RadioGroup>
+
+                    <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+                        {coins.map((i) => (
+                            <CoinCard
+                                id={i.id}
+                                key={i.id}
+                                name={i.name}
+                                img={i.image}
+                                price={i.current_price}
+                                symbol={i.symbol}
+                                currencySymbol={currencySymbol}
+                            />
+                        ))}
+                    </HStack>
+
+                    <HStack w={"full"} overflowX={"auto"} p={"8"}>
+                        {
+                            btns.map((item, index) => (
+                                <Button
+                                    bgColor={"blackAlpha.900"}
+                                    color={"white"}
+                                    onClick={() => changePage(index + 1)}
+                                >
+                                    {index + 1}
+                                </Button>
+                            ))
+                        }
+                    </HStack>
+                </>
+            )}
+        </Container>
+    )
+}
+
+
+// export default Exchanges
+export default Coins
